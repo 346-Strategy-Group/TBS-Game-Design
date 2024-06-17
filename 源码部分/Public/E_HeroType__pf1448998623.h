660d9490f75e072467acf4ec70c499b5a146dba8
Darli000000 <149639984+Darli000000@users.noreply.github.com>
2024-06-17 15:43:46 +0800

    Revert "上传源码"
    This reverts commit bc8a4e24bd6bd6c98a2267eb10c04f2fbd2497a8.

--- 源码部分/Public/E_HeroType__pf1448998623.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "E_HeroType__pf1448998623.generated.h"
-UENUM(BlueprintType, meta=(ReplaceConverted="/Game/Art/Heros/E_HeroType.E_HeroType", OverrideNativeName="E_HeroType", EnumDisplayNameFn="E__E_HeroType__pf__GetUserFriendlyName") )
-enum class E__E_HeroType__pf  : uint8
-{
-	NewEnumerator0 = 0 UMETA(OverrideName = "E_HeroType::NewEnumerator0"),
-	E__E_HeroType__pf_MAX = 1 UMETA(OverrideName = "E_HeroType::E_MAX"),
-};
-FText E__E_HeroType__pf__GetUserFriendlyName(int32 InValue);
